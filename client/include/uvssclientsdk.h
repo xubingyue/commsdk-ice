dfdda718e7d87f496e75212c648ecc6376108353
hfrwx <hf1rwx@163.com>
2017-03-10 15:00:57 +0800

    export macros

--- client/include/uvssclientsdk.h
+++ client/include/uvssclientsdk.h
@@ -1,19 +1,29 @@
 #ifndef UVSSCLIENTSDK_H
 #define UVSSCLIENTSDK_H
 
+#ifdef _WIN32
+    #ifdef UVSS_COMM_EXPORTS
+        #define UVSS_COMM_API _declspec(dllexport)
+    #else
+        #define UVSS_COMM_API _declspec(dllimport)
+    #endif
+#else
+    #define UVSS_COMM_API
+#endif
+
 #include <clienti.h>
 
 typedef ClientConnectionInfoCallback UVSSMessageCallback;
 typedef ClientCheckInfoCallback UVSSCheckInfoCallback;
 
 extern "C" {
-    void SetUVSSMessageCallback(UVSSMessageCallback);
-    void SetUVSSCheckInfoCallback(UVSSCheckInfoCallback);
-    int UVSSInitialize();
-    void UVSSUninitialize();
-    int UVSSConnect(const char*, int);
-    int UVSSDisconnect(int);
-    const char* GetUVSSVersion();
+    UVSS_COMM_API void SetUVSSMessageCallback(UVSSMessageCallback);
+    UVSS_COMM_API void SetUVSSCheckInfoCallback(UVSSCheckInfoCallback);
+    UVSS_COMM_API int UVSSInitialize();
+    UVSS_COMM_API void UVSSUninitialize();
+    UVSS_COMM_API int UVSSConnect(const char*, int);
+    UVSS_COMM_API int UVSSDisconnect(int);
+    UVSS_COMM_API const char* GetUVSSVersion();
 }
 
 #endif
--- server/include/uvssserversdk.h
+++ server/include/uvssserversdk.h
@@ -1,18 +1,28 @@
 #ifndef UVSSSERVERSDK_H
 #define UVSSSERVERSDK_H
 
+#ifdef _WIN32
+    #ifdef UVSS_COMM_EXPORTS
+        #define UVSS_COMM_API _declspec(dllexport)
+    #else
+        #define UVSS_COMM_API _declspec(dllimport)
+    #endif
+#else
+    #define UVSS_COMM_API
+#endif
+
 #include <serveri.h>
 
 typedef ServerConnectionInfoCallback UVSSServerCallback;
 
 extern "C" {
-    void SetUVSSServerCallback(UVSSServerCallback);
-    void SetUVSSServerPort(int);
-    int InitUVSSServer();
-    void UninitUVSSServer();
-    void SendUVSSCheckInfo(const char*, const char*,
+    UVSS_COMM_API void SetUVSSServerCallback(UVSSServerCallback);
+    UVSS_COMM_API void SetUVSSServerPort(int);
+    UVSS_COMM_API int InitUVSSServer();
+    UVSS_COMM_API void UninitUVSSServer();
+    UVSS_COMM_API void SendUVSSCheckInfo(const char*, const char*,
             const char*, const char*, const char*, const char*, const char*);
-    const char* GetUVSSServerVersion();
+    UVSS_COMM_API const char* GetUVSSServerVersion();
 }
 
 #endif
